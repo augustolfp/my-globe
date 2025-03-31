c7fb4a84193402e51e477a1a7940feb46fa0376e
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 17:02:59 -0300

    feat: center content on page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
-    <p className="container mx-auto flex flex-col justify-center items-center h-screen">
-      <Homepage />
-    </p>
+    <div className="container mx-auto flex flex-col justify-center items-center h-screen">
+      <div className="bg-purple-400 w-3xl aspect-square">
+        <Homepage />
+      </div>
+    </div>
   );
 }
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,3 +1,3 @@
 export default function Homepage() {
-  return <p className="bg-purple-400">Homepage!</p>;
+  return <div>Homepage!</div>;
 }
