ffcefb14f88c632f8d93062b7008121f8e9cfd8d
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 18:15:10 -0300

    feat: create 3D cartesian components

--- src/components/Dot/index.tsx
+++ src/components/Dot/index.tsx
@@ -1,11 +1,12 @@
 interface Props {
   x: string;
   y: string;
+  z: string;
 }
 
-export default function Dot({ x, y }: Props) {
+export default function Dot({ x, y, z }: Props) {
   return (
-    <div style={{ position: "absolute", left: x, bottom: y }}>
+    <div style={{ position: "absolute", left: y, bottom: z }}>
       <div className="w-2 aspect-square bg-orange-400 -translate-x-1/2 translate-y-1/2"></div>
     </div>
   );
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -19,11 +19,54 @@ export default function Homepage() {
       y: 500,
     },
   ];
+
+  const square3D = [
+    {
+      x: 0,
+      y: 0,
+      z: 0,
+    },
+    {
+      x: 500,
+      y: 0,
+      z: 0,
+    },
+    {
+      x: 500,
+      y: 0,
+      z: 500,
+    },
+    {
+      x: 0,
+      y: 0,
+      z: 500,
+    },
+    {
+      x: 0,
+      y: 500,
+      z: 0,
+    },
+    {
+      x: 500,
+      y: 500,
+      z: 0,
+    },
+    {
+      x: 0,
+      y: 500,
+      z: 500,
+    },
+    {
+      x: 500,
+      y: 500,
+      z: 500,
+    },
+  ];
+
   return (
     <div className="bg-purple-400 w-[1000px] aspect-square relative">
-      <Dot x="500px" y="500px" />
-      {square2D.map(({ x, y }) => (
-        <Dot x={`${x}px`} y={`${y}px`} />
+      {square3D.map(({ x, y, z }) => (
+        <Dot x={`${x}px`} y={`${y}px`} z={`${z}px`} />
       ))}
     </div>
   );
