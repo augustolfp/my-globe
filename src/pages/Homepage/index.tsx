8fba309251bd92b9860b4fd94a5e6e3678b79ecd
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 18:05:36 -0300

    feat: render 2-dimensional square scatter

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,9 +1,30 @@
 import Dot from "../../components/Dot";
 
 export default function Homepage() {
+  const square2D = [
+    {
+      x: 0,
+      y: 0,
+    },
+    {
+      x: 500,
+      y: 0,
+    },
+    {
+      x: 0,
+      y: 500,
+    },
+    {
+      x: 500,
+      y: 500,
+    },
+  ];
   return (
     <div className="bg-purple-400 w-[1000px] aspect-square relative">
       <Dot x="500px" y="500px" />
+      {square2D.map(({ x, y }) => (
+        <Dot x={`${x}px`} y={`${y}px`} />
+      ))}
     </div>
   );
 }
