ce048dd9078064045a6b3760a583a2d3b30e4de6
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 20:30:20 -0300

    feat: add geoCoordinates json

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,71 +1,8 @@
 import Dot from "../../components/Dot";
-import coordinates from "../../assets/coordinates.json";
+import cartesianCoordinates from "../../assets/cartesianCoordinates.json";
+import geoCoordinates from "../../assets/geoCoordinates.json";
 
 export default function Homepage() {
-  console.log(coordinates[0]);
-  const square2D = [
-    {
-      x: 0,
-      y: 0,
-    },
-    {
-      x: 500,
-      y: 0,
-    },
-    {
-      x: 0,
-      y: 500,
-    },
-    {
-      x: 500,
-      y: 500,
-    },
-  ];
-
-  const square3D = [
-    {
-      x: 0,
-      y: 0,
-      z: 0,
-    },
-    {
-      x: 500,
-      y: 0,
-      z: 0,
-    },
-    {
-      x: 0,
-      y: 500,
-      z: 0,
-    },
-    {
-      x: 500,
-      y: 500,
-      z: 0,
-    },
-
-    {
-      x: 0,
-      y: 0,
-      z: -500,
-    },
-    {
-      x: 500,
-      y: 0,
-      z: -500,
-    },
-    {
-      x: 0,
-      y: 500,
-      z: -500,
-    },
-    {
-      x: 500,
-      y: 500,
-      z: -500,
-    },
-  ];
-
   return (
     <div
       className="border-solid border-2 border-purple-300 w-[700px] aspect-square relative"
@@ -75,7 +12,7 @@ export default function Homepage() {
         transform: "perspective(1240px)",
       }}
     >
-      {coordinates.map(({ x, y, z }) => (
+      {cartesianCoordinates.map(({ x, y, z }) => (
         <Dot
           x={Math.round(5 * x)}
           y={5 * Math.round(y)}
