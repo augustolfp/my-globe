f05116c07d69aab1d29a335adb3bab0f1e6c779e
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 20:47:33 -0300

    feat: adjust plot

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,20 +1,13 @@
 import Dot from "../../components/Dot";
-// import cartesianCoordinates from "../../assets/cartesianCoordinates.json";
+import cartesianCoordinates from "../../assets/cartesianCoordinates.json";
 // import geoCoordinates from "../../assets/geoCoordinates.json";
 
 export default function Homepage() {
-  const points = [
-    {
-      x: 100,
-      y: 150,
-      z: 10,
-    },
-  ];
   return (
     <div className="bg-black w-4 aspect-square relative" style={{}}>
       <p className="absolute top-4 left-6 font-bold">Origem</p>
-      {points.map(({ x, y, z }) => (
-        <Dot x={x} y={y} z={z} />
+      {cartesianCoordinates.map(({ x, y, z }) => (
+        <Dot x={-10 * x} y={-10 * z} z={10 * y} />
       ))}
     </div>
   );
