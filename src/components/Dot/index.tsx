94ab0e73bdb364ae8215e2600b2dc844f2d7063f
Augusto Lopes Falcão Pinheiro <augustolfp@gmail.com>
2025-03-31 22:06:35 -0300

    feat: implement variable opacity

--- src/components/Dot/index.tsx
+++ src/components/Dot/index.tsx
@@ -2,9 +2,10 @@ interface Props {
   x: number;
   y: number;
   z: number;
+  opacity: number;
 }
 
-export default function Dot({ x, y, z }: Props) {
+export default function Dot({ x, y, z, opacity }: Props) {
   return (
     <div
       style={{
@@ -12,6 +13,7 @@ export default function Dot({ x, y, z }: Props) {
         top: 0,
         left: 0,
         transform: `translate3d(${x}px,${y}px, ${z}px)`,
+        opacity: opacity,
       }}
     >
       <div className="w-2 aspect-square bg-orange-400"></div>
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -3,11 +3,22 @@ import cartesianCoordinates from "../../assets/cartesianCoordinates.json";
 // import geoCoordinates from "../../assets/geoCoordinates.json";
 
 export default function Homepage() {
+  const radius = Math.min(...cartesianCoordinates.map(({ y }) => y));
   return (
-    <div className="bg-black w-4 aspect-square relative" style={{}}>
+    <div
+      className="bg-black w-4 aspect-square relative"
+      style={{
+        perspective: "5000px",
+      }}
+    >
       <p className="absolute top-4 left-6 font-bold">Origem</p>
       {cartesianCoordinates.map(({ x, y, z }) => (
-        <Dot x={-10 * x} y={-10 * z} z={10 * y} />
+        <Dot
+          x={-10 * x}
+          y={-10 * z}
+          z={10 * y}
+          opacity={y > 0 ? 1 : Math.abs((radius - y) / radius)}
+        />
       ))}
     </div>
   );
